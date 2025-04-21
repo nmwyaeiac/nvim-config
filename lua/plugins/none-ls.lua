709e0ae73af6f3af0edc1317e26039c551d9e960
nmwyaeiac <161044986+nmwyaeiac@users.noreply.github.com>
2025-04-21 15:28:04 +0200

    Update none-ls.lua

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -30,17 +30,17 @@ return {
       -- C/C++
       try_add(formatting.clang_format)
       try_add(diagnostics.clang_check)
-      try_add(diagnostics.cppcheck) -- Ajout pour une meilleure analyse C/C++
+      try_add(diagnostics.cppcheck)
 
       -- Java 
       try_add(formatting.google_java_format)
-      try_add(diagnostics.checkstyle) -- Ajout pour les diagnostics Java
+      try_add(diagnostics.checkstyle)
       
       -- Python
       try_add(formatting.black)
-      try_add(diagnostics.flake8) -- Décommenté pour Python
-      try_add(diagnostics.pylint) -- Ajout pour une analyse plus complète
-      try_add(diagnostics.mypy)   -- Analyse de type pour Python
+      try_add(diagnostics.flake8)
+      try_add(diagnostics.pylint)
+      try_add(diagnostics.mypy)
       
       -- Web
       try_add(formatting.prettier)
@@ -59,17 +59,20 @@ return {
       
       -- Bash
       try_add(formatting.shfmt)
-      try_add(diagnostics.shellcheck) -- Décommenté pour Bash
+      try_add(diagnostics.shellcheck)
       
       -- Lua
       try_add(formatting.stylua)
       try_add(diagnostics.luacheck)
       
-      -- Rust
-      try_add(formatting.rustfmt)
+      -- Rust - Correction pour rustfmt
+      -- Utilisation explicite avec les options correctes
+      try_add(formatting.rustfmt.with({
+        extra_args = {"--edition", "2021"},
+      }))
       
       null_ls.setup({
-        debug = true, -- Activez le débogage pour voir les erreurs potentielles
+        debug = true,
         sources = sources,
         on_attach = function(client, bufnr)
           -- Format on save (optionnel)
@@ -94,17 +97,27 @@ return {
       })
 
       -- Configuration language-specific LSP
+      -- Séparation de la configuration LSP de la configuration de formatage
       lspconfig.rust_analyzer.setup({
         settings = {
           ["rust-analyzer"] = {
             checkOnSave = { command = "clippy" },
-            formatting = { enable = true },
+            -- Enlever l'option formatting ici pour éviter les conflits
           },
         },
+        -- Assurez-vous que rust_analyzer ne force pas le formatage
+        on_attach = function(client, bufnr)
+          -- Désactiver le formatage intégré de rust-analyzer
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+        end,
       })
       
       -- Raccourci format
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format code" })
+      vim.keymap.set("n", "<leader>gf", function()
+        vim.lsp.buf.format({ timeout_ms = 2000 })
+      end, { desc = "Format code" })
+      
       -- Raccourci pour voir les diagnostics
       vim.keymap.set("n", "<leader>dl", vim.diagnostic.open_float, { desc = "Afficher diagnostic" })
     end,
