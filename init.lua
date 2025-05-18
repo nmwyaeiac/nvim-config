69a3415f5673e03022fc72b0994bcac039b017b1
nmwyaeiac <161044986+nmwyaeiac@users.noreply.github.com>
2025-05-18 14:18:07 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,25 +1,26 @@
--- init.lua  
+-- init.lua (version mise à jour)
 -- Installation automatique de lazy.nvim s'il n'est pas présent
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Configuration de base de Vim/Neovim
 require("vim-config")
 
--- Désactiver explicitement les providers inutilisés pour éviter les avertissements
+-- Désactiver explicitement les providers inutilisés
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
--- Définir le chemin Python si disponible
+
+-- Définir le chemin Python
 if vim.fn.executable("python3") == 1 then
   vim.g.python3_host_prog = vim.fn.exepath("python3")
 else
@@ -27,48 +28,56 @@ else
   vim.g.loaded_python3_provider = 0
 end
 
+-- Appliquer les patches de compatibilité avant le chargement des plugins
+-- Cela corrigera les avertissements de déprécation
+local init_patch = require("init-patch")
+init_patch.apply_compatibility_patches()
+
 -- Chargement des raccourcis clavier généraux
 require("keymaps")
 
 -- Installer les plugins avec lazy.nvim
 require("lazy").setup("plugins", {
-	defaults = { lazy = false },
-	install = { colorscheme = { "sonokai" } },
-	checker = { enabled = true, notify = false },
-	change_detection = {
-		notify = false,
-	},
-	ui = {
-		border = "rounded",
-	},
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"gzip",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
-		},
-	},
+  defaults = { lazy = false },
+  install = { colorscheme = { "sonokai" } },
+  checker = { enabled = true, notify = false },
+  change_detection = {
+    notify = false,
+  },
+  ui = {
+    border = "rounded",
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
 })
 
 -- Configuration supplémentaire après le chargement des plugins
 vim.api.nvim_create_autocmd("User", {
-	pattern = "LazyDone",
-	callback = function()
-		-- Charger la configuration des débogueurs après l'installation des plugins
-		if package.loaded["dap"] and package.loaded["dapui"] then
-			require("keymaps.debug").setup()
-		end
+  pattern = "LazyDone",
+  callback = function()
+    -- Charger la configuration des débogueurs après l'installation des plugins
+    if package.loaded["dap"] and package.loaded["dapui"] then
+      require("keymaps.debug").setup()
+    end
 
-		-- Charger la configuration de navigation après l'installation des plugins
-		if package.loaded["telescope"] and package.loaded["neo-tree"] then
-			require("keymaps.navigation").setup()
-		end
+    -- Charger la configuration de navigation après l'installation des plugins
+    if package.loaded["telescope"] and package.loaded["neo-tree"] then
+      require("keymaps.navigation").setup()
+    end
+    
+    -- Vérifier les dépendances manquantes
+    init_patch.check_missing_dependencies()
 
-		-- Message de démarrage
-		print("Configuration NeoVim chargée avec succès!")
-	end,
+    -- Message de démarrage
+    print("Configuration NeoVim chargée avec succès!")
+  end,
 })
