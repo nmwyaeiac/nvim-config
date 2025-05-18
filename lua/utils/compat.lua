dfbeaee2fb141ef83edf3d8804b685f327672a93
nmwyaeiac <161044986+nmwyaeiac@users.noreply.github.com>
2025-05-18 14:15:43 +0200

    Update compat.lua

--- lua/utils/compat.lua
+++ lua/utils/compat.lua
@@ -1,4 +1,4 @@
--- lua/utils/compat.lua
+-- lua/utils/compat-improved.lua
 -- Fonctions de compatibilité pour gérer les avertissements liés aux fonctions obsolètes
 local M = {}
 
@@ -12,9 +12,53 @@ function M.tbl_flatten(tbl)
 end
 
 -- Wrapper pour vim.validate (obsolète depuis Neovim 1.0)
+-- Cette fonction est conçue pour être un remplacement direct
+-- qui évite les avertissements tout en gardant la même fonctionnalité
 function M.validate(...)
-  -- Utilisation de la fonction validate sans avertissement
-  return vim.validate(...)
+  -- Désactiver temporairement les avertissements de dépréciation
+  local old_deprecated_fn = vim._with_meta and vim._with_meta.deprecated_fn
+  if old_deprecated_fn then
+    vim._with_meta.deprecated_fn = function() end
+  end
+  
+  -- Appeler la fonction originale
+  local result = vim.validate(...)
+  
+  -- Restaurer le comportement d'avertissement
+  if old_deprecated_fn then
+    vim._with_meta.deprecated_fn = old_deprecated_fn
+  end
+  
+  return result
+end
+
+-- Fonction pour patcher les plugins qui utilisent vim.validate
+-- Cette fonction est utile pour les plugins que nous ne pouvons pas modifier directement
+function M.patch_validate_calls()
+  -- Sauvegarder la fonction originale
+  local original_validate = vim.validate
+  
+  -- Remplacer par notre version sans avertissement
+  vim.validate = M.validate
+  
+  -- Retourner une fonction pour restaurer l'original si nécessaire
+  return function()
+    vim.validate = original_validate
+  end
+end
+
+-- Fonction pour patcher les plugins qui utilisent vim.tbl_flatten
+function M.patch_tbl_flatten_calls()
+  -- Sauvegarder la fonction originale
+  local original_tbl_flatten = vim.tbl_flatten
+  
+  -- Remplacer par notre version compatible
+  vim.tbl_flatten = M.tbl_flatten
+  
+  -- Retourner une fonction pour restaurer l'original si nécessaire
+  return function()
+    vim.tbl_flatten = original_tbl_flatten
+  end
 end
 
 return M
